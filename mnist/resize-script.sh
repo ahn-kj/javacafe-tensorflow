2c1c1635af9b7beb3dd0e5ae4c640e8a3c3d657f
CHO <rygh4775@gmail.com>
2016-10-22 16:56:28 +0900

    리사이즈 대상 디렉토리 변경
    리사이즈 대상
     - 상위 upload 디렉토리에 있는 *.png, *.jpg

--- mnist/resize-script.sh
+++ mnist/resize-script.sh
@@ -3,11 +3,20 @@
 #simple script for resizing images in all class directories
 #also reformats everything from whatever to png
 
-if [ `ls test-images/*.jpg 2> /dev/null | wc -l ` -gt 0 ]; then
-  echo hi
-  for file in test-images/*.jpg; do
-    convert "$file" -resize 28x28\! "${file%.*}.png"
-    file "$file" #uncomment for testing
-    rm "$file"
-  done
+if [ `ls ../upload/*.jpg 2> /dev/null | wc -l ` -gt 0 ]; then
+echo hi
+for file in ../upload/*.jpg; do
+convert "$file" -resize 28x28\! "${file%.*}.png"
+file "$file" #uncomment for testing
+rm "$file"
+done
 fi
+
+if [ `ls ../upload/*.png 2> /dev/null | wc -l ` -gt 0 ]; then
+echo hi
+for file in ../upload/*.png; do
+convert "$file" -resize 28x28\! "${file%.*}.png"
+file "$file" #uncomment for testing
+done
+fi
+
