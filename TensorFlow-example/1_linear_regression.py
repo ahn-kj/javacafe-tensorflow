8cbc6f6d642f82cf3db681478c31e2a0f13947f7
유미령 <kioku714@gmail.com>
2016-11-09 23:16:12 +0900

    발표 시 사용할 예제코드 추가 중

---
+++ TensorFlow-example/1_linear_regression.py
@@ -0,0 +1,29 @@
+import tensorflow as tf
+
+x_data = [54, 8, 30, 24, 46, 12, 20, 37, 40, 48]
+y_data = [800, 320, 600, 630, 700, 680, 730, 720, 700, 920]
+
+W = tf.Variable(tf.random_uniform([1], -1.0, 1.0))
+b = tf.Variable(tf.random_uniform([1], -1.0, 1.0))
+
+X = tf.placeholder(tf.float32)
+Y = tf.placeholder(tf.float32)
+
+hypothesis = W * X + b
+
+cost = tf.reduce_mean(tf.square(hypothesis - Y))
+
+a = tf.Variable(0.1)
+optimizer = tf.train.GradientDescentOptimizer(a)
+train = optimizer.minimize(cost)
+
+init = tf.initialize_all_variables()
+
+sess = tf.Session()
+sess.run(init)
+
+for step in xrange(2001):
+    sess.run(train, feed_dict={X: x_data, Y: y_data})
+
+    if step % 20 == 0:
+        print step, sess.run(cost, feed_dict={X: x_data, Y: y_data}), sess.run(W), sess.run(b)
