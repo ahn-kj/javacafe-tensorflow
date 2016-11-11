a9003ed6d6a246d058f9a7fc50ee788de8f6efc8
akjhh <rnlwjd0613@gmail.com>
2016-11-11 12:39:03 +0900

    발표예제 주석 추가

--- TensorFlow-example/1_linear_regression.py
+++ TensorFlow-example/1_linear_regression.py
@@ -1,19 +1,31 @@
+#-*- coding: utf-8 -*-
+
 import tensorflow as tf
 
+# ======== 학습데이터 ========
+# 학습시간
 x_data = [54, 8, 30, 24, 46, 12, 20, 37, 40, 48]
+# 토익점수
 y_data = [800, 320, 600, 630, 700, 680, 730, 720, 700, 920]
 
+# ======== 초기값 설정 =========
 W = tf.Variable(tf.random_uniform([1], -1.0, 1.0))
 b = tf.Variable(tf.random_uniform([1], -1.0, 1.0))
 
+# ======== 변수 설정 ========
 X = tf.placeholder(tf.float32)
 Y = tf.placeholder(tf.float32)
 
+# ======== Linear Regression 에서 학습될 가설 ========
 hypothesis = W * X + b
 
-cost = tf.reduce_mean(tf.square(hypothesis - Y) + 1e-10)
+# ======== Linear Regression 에서 학습될 가설의 Cost Function ========
+cost = tf.reduce_mean(tf.square(hypothesis - Y))
 
+# ======== Gradient Descent Algorithm 에서 Step ========
 learning_rate = 0.0008
+
+# ======== 텐서플로우에 내장된 GradientDescentOptimizer ========
 optimizer = tf.train.GradientDescentOptimizer(learning_rate).minimize(cost)
 
 init = tf.initialize_all_variables()
@@ -21,11 +33,13 @@ init = tf.initialize_all_variables()
 sess = tf.Session()
 sess.run(init)
 
-for step in xrange(2000):
+# ======== 학습 시작 ========
+for step in xrange(20000):
     sess.run(optimizer, feed_dict={X: x_data, Y: y_data})
 
     if step % 20 == 0:
         print step, sess.run(cost, feed_dict={X: x_data, Y: y_data}), sess.run(W), sess.run(b)
 
-print sess.run(hypothesis, feed_dict={X: 10})
-print sess.run(hypothesis, feed_dict={X: 20})
+# ======== 학습된 우리의 프로그램에 예측 문의 ========
+print "10시간:" + str(sess.run(hypothesis, feed_dict={X: 10}))
+print "40시간:" + str(sess.run(hypothesis, feed_dict={X: 40}))
